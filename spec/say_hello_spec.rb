a23b348abd60f71017adfb5d5997f1492eaf04ee
steadfast-creator-0010 <jpchigne@gmail.com>
2019-07-28 17:54:05 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -7,8 +7,8 @@ describe "say_hello" do
     say_hello("Kent Beck")
   end
 
-  # it 'defaults to Ruby Programmer when no name is passed in' do
-  #   expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
-  #   say_hello()
-  # end
+  it 'defaults to Ruby Programmer when no name is passed in' do
+    expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
+     say_hello()
+  end
 end
